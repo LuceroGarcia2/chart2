ce05ae7eb5eb4a916de8de62b8ae08c78c865378
LuceroGarcia2 <159559301+LuceroGarcia2@users.noreply.github.com>
2024-06-18 22:38:01 -0700

    charts2

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import BarChart from './Charts/BarChart';
+
+
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+      { <DoughnutChart /> }
+      { <LineChart /> }
+      { <PieChart />}
+    </div>
+  );
+}
+
+export default App;
